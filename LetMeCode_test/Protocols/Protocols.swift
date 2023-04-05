23fd15a4c9bd4c67109b2d2901a4f5a66411bcab
Алексей Чайкин <barjastroy@gmail.com>
2023-04-05 22:02:58 +0300

    Create Protocols.swift

---
+++ LetMeCode_test/Protocols/Protocols.swift
@@ -0,0 +1,24 @@
+//
+//  Protocols.swift
+//  LetMeCode_test
+//
+//  Created by macbook on 05.04.2023.
+//
+
+import Foundation
+
+protocol APICallerProtocol {
+    var urlInfo: UrlInfoProtocol { get }
+    var isPaginating: Bool { get set }
+    
+    func getTopStories(pagination: Bool, completion: @escaping (Result<[Article], Error>) -> Void)
+}
+
+protocol UrlInfoProtocol {
+    var currentURL: URL? { get set }
+    var pageSize: Int { get set }
+    var page: Int { get set }
+    var apiKey: String { get set }
+    
+    func getNextPageURL() -> URL?
+}
