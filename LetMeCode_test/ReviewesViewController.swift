5a2456670378d1ac8ab758ef97798db485a2f7d9
Алексей Чайкин <barjastroy@gmail.com>
2023-04-06 18:28:26 +0300

    Create ReviewesViewController.swift

---
+++ LetMeCode_test/ReviewesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReviewesViewController.swift
+//  LetMeCode_test
+//
+//  Created by macbook on 06.04.2023.
+//
+
+import UIKit
+
+protocol ReviewesViewProtocol {
+    
+}
+
+class ReviewesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   
+    }
+    
+
+    
+
+}
+
+extension ReviewesViewController: ReviewesViewProtocol {
+    
+}
