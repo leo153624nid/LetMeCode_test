2f7991abf6b2fc69f1a975d91692a0bd80da89f6
Алексей Чайкин <barjastroy@gmail.com>
2023-04-06 20:00:07 +0300

    Create CriticsModuleBuilder.swift

---
+++ LetMeCode_test/CriticsModuleBuilder.swift
@@ -0,0 +1,26 @@
+//
+//  CriticsModuleBuilder.swift
+//  LetMeCode_test
+//
+//  Created by macbook on 06.04.2023.
+//
+
+import Foundation
+import UIKit
+
+class CriticsModuleBuilder {
+    static func build() -> UIViewController { // change to CriticsViewController
+//        let interactor = ReviewesInteractor(with: APICaller.shared)
+//        let router = ReviewesRouter()
+//        let presenter = ReviewesPresenter(router: router, interactor: interactor)
+//        let viewController = ReviewesViewController()
+//
+//        viewController.presenter = presenter
+//        presenter.view = viewController
+//        interactor.presenter = presenter
+//        router.viewController = viewController
+        
+//        return viewController
+        return UIViewController()
+    }
+}
