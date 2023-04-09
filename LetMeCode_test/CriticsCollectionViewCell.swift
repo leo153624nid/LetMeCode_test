0b266b3bed2e85b4ce8121cc072c68fef3492e08
Алексей Чайкин <barjastroy@gmail.com>
2023-04-09 16:47:29 +0300

    Update CriticsCollectionViewCell.swift

--- LetMeCode_test/CriticsCollectionViewCell.swift
+++ LetMeCode_test/CriticsCollectionViewCell.swift
@@ -10,19 +10,6 @@ import UIKit
 class CriticsCollectionViewCell: UICollectionViewCell {
     static let identifier = "CriticsCollectionViewCell"
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 18, weight: .semibold)
-        return label
-    }()
-    private let subTitleLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 13, weight: .light)
-        label.textAlignment = .left
-        return label
-    }()
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.layer.cornerRadius = 6
@@ -32,26 +19,18 @@ class CriticsCollectionViewCell: UICollectionViewCell {
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
-    private let bylineLabel: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 13, weight: .semibold)
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        label.textAlignment = .center
         return label
     }()
-    private let dateLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 13, weight: .light)
-        return label
-    }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.addSubview(titleLabel)
-        contentView.addSubview(subTitleLabel)
         contentView.addSubview(imageView)
-        contentView.addSubview(bylineLabel)
-        contentView.addSubview(dateLabel)
+        contentView.addSubview(titleLabel)
     }
     
     required init?(coder: NSCoder) {
@@ -60,47 +39,27 @@ class CriticsCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        imageView.frame = CGRect(
+            x: 5,
+            y: 5,
+            width: contentView.frame.size.width - 10,
+            height: contentView.frame.size.height - 30)
         titleLabel.frame = CGRect(
             x: 10,
-            y: 10,
-            width: contentView.frame.size.width - 170,
-            height: 30)
-        subTitleLabel.frame = CGRect(
-            x: 10,
-            y: 30,
-            width: contentView.frame.size.width - 170,
-            height: contentView.frame.size.height - 80)
-        bylineLabel.frame = CGRect(
-            x: 10,
-            y: contentView.frame.size.height - 60,
-            width: contentView.frame.size.width - 170,
-            height: 30)
-        dateLabel.frame = CGRect(
-            x: 10,
             y: contentView.frame.size.height - 30,
-            width: contentView.frame.size.width - 170,
-            height: 20)
-        imageView.frame = CGRect(
-            x: contentView.frame.size.width - 150,
-            y: 5,
-            width: 140,
-            height: contentView.frame.size.height - 10)
+            width: contentView.frame.size.width - 10,
+            height: 30)
         contentView.backgroundColor = .white
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         titleLabel.text = nil
-        subTitleLabel.text = nil
-        bylineLabel.text = nil
         imageView.image = nil
-        dateLabel.text = nil
     }
     
     func configure(with viewModel: CriticsCollectionViewCellViewModel) {
         titleLabel.text = viewModel.title
-        subTitleLabel.text = viewModel.status
-        bylineLabel.text = viewModel.bio
         
         // image from cache
         if let data = viewModel.imageData {
